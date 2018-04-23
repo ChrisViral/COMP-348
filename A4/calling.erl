a889eca9b82412c5e29aba9c2ee21e08b65675bd
Christophe Savard <christophe_savard@hotmail.ca>
2018-04-23 19:22:23 -0400

    Update calling.erl

--- A4/calling.erl
+++ A4/calling.erl
@@ -1,65 +1,62 @@
-%% Christophe Savard
-%% 40017812
-%% COMP-348 E
-%% Assignment 4
-%% April 22nd 2018
+%%% Christophe Savard
+%%% 40017812
+%%% COMP-348 E
+%%% Assignment 4
+%%% April 23rd 2018
 
 -module(calling).
--export([call_all/2]).
+-export([call_all/3]).
 
+-import(erlang, [timestamp/0]).
 -import(lists, [foreach/2]).
--import(random, [uniform/1]).
+-import(rand, [uniform/1]).
 -import(timer, [sleep/1]).
 
-%% @doc Calls all the names in the passed list then listens for replies
-%% @param Name Name of the caller
-%% @param Calls List of names to call
--spec call_all(atom(), list(atom())) -> ok.
-call_all(Name, Calls) ->
-  % Register thread
-  register(Name, self()),
-
-  % Send a message to the Main thread to say it has been registered
-  whereis(main) ! ok,
-
+%%% @doc Calls all the names in the passed list then listens for replies
+%%% @param Name Name of the caller
+%%% @param Calls List of names to call
+%%% @param PID of the main thread
+-spec call_all(atom(), list(atom()), pid()) -> ok.
+call_all(Name, Calls, Main) ->
   % Wait for master to signal that all threads have been initialized
   receive
-    _ -> ok
+    Main -> continue
   end,
 
   % Send all calls
-  foreach(fun(C) -> whereis(C) ! {Name} end, Calls),
+  foreach(fun(C) -> whereis(C) ! {call, self(), Name} end, Calls),
 
   % Listen to calls and replies
-  listen(Name).
+  listen(Name, Main).
 
 %% @doc Listens to calls, replies to them, and informs the main thread of activity
 %% @param Name Name of the caller
--spec listen(atom()) -> ok.
-listen(Name) ->
+%% @param Main PID of the main thread
+-spec listen(atom(), pid()) -> ok.
+listen(Name, Main) ->
   receive
     %Receiving a call
-    {Caller} ->
-      % Get timestamp
-      {_, _, Time} = now(),
+    {call, PID, Caller} ->
+      % Get timestamp (erlang:now() is marked as deprecated, documentation says to use timestamp() instead)
+      {_, _, Time} = timestamp(),
       % Sleep a random amount of time
       sleep(uniform(100)),
-      % Signal Main thread
-      whereis(main) ! {call, Caller, Name, Time},
       % Send reply
-      whereis(Caller) ! {Name, Time},
-      listen(Name);
+      PID ! {reply, Name, Time},
+      % Signal Main thread
+      Main ! {call, Caller, Name, Time},
+      listen(Name, Main);
 
     % Receiving a reply
-    {Replier, Time} ->
+    {reply, Replier, Time} ->
       % Sleep a random amount of time
       sleep(uniform(100)),
       % Signal main thread
-      whereis(main) ! {reply, Replier, Name, Time},
-      listen(Name)
+      Main ! {reply, Replier, Name, Time},
+      listen(Name, Main)
 
   % Timeout
   after 1000 ->
     % Signal main thread of process completion
-    whereis(main) ! Name
+    Main ! {finished, Name}
   end.
